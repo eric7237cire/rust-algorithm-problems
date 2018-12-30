387017d05224f8a2a8f85bb2222e145c8c94d975
eric7237cire <eric7237cire@gmail.com>
2018-12-30 16:13:21 +0100

    more kata

--- codewars/src/seven_kyu.rs
+++ codewars/src/seven_kyu.rs
@@ -62,3 +62,37 @@ fn new_avg_tests() {
   let a4 = [14000.25, 300.76, 50.56, 70.0, 90.0, 11.0, 150.48, 1200.98];
   test_new_avg(&a4, 4800.0, Some(27326));
 }
+
+fn to_leet_speak(s: &str) -> String {
+    s.chars().map(|c| match c {        
+  'A' => '@', 'B' => '8', 'C' => '(', 'E' => '3', 
+  'G' => '6', 'H' => '#', 'I' => '!', 'L' => '1',
+  'O' => '0', 'S' => '$', 'T' => '7', 'Z' => '2',
+  other => other
+    }).collect()
+}
+
+#[test]
+fn leet() {
+    assert_eq!(to_leet_speak("LEET"), "1337");
+}
+
+#[test]
+fn codewars() {
+    assert_eq!(to_leet_speak("CODEWARS"), "(0D3W@R$");
+}
+
+#[test]
+fn hello_world() {
+    assert_eq!(to_leet_speak("HELLO WORLD"), "#3110 W0R1D");
+}
+
+#[test]
+fn lorem_ipsum() {
+    assert_eq!(to_leet_speak("LOREM IPSUM DOLOR SIT AMET"), "10R3M !P$UM D010R $!7 @M37");
+}
+
+#[test]
+fn quick_brown_fox() {
+    assert_eq!(to_leet_speak("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"), "7#3 QU!(K 8R0WN F0X JUMP$ 0V3R 7#3 1@2Y D06");
+}
--- codewars/src/six_kyu.rs
+++ codewars/src/six_kyu.rs
@@ -37,3 +37,74 @@ fn basics_longest_consec() {
     testing_longest_consec(vec!["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15, "");
     testing_longest_consec(vec!["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0, "");
 }
+
+/*
+mine, 1 liner, O(N) 
+fn solution(num: i32) -> i32 {
+  (1..num).filter( |n| n % 3 == 0 || n % 5 == 0 ).sum()
+}
+*/
+
+//Solution of https://www.codewars.com/kata/reviews/58d85666eda71d6e40000002/groups/5a0edccc27965c26f4003218 which is O(1)
+fn solution(max: i32) -> i32 {
+  // Result is equals to 
+  // sum of numbers divided by 3 + sum of numbers divided by 5 - sum of numbers divided by 3 and 5
+  
+  sumOfSequence(3, max) + sumOfSequence(5, max) - sumOfSequence(15, max)
+}
+
+fn sumOfSequence(divider: i32, max: i32) -> i32 {
+  // Result is sum of a sequence X, X * 2, X * 3, X * 4, ..., X * N
+  // which is also equals to X * (1 + 2 + 3 + 4 + ... + N)
+  // Where X is a divider and N is a highest number satisfying the
+  // condition X * N < max, so N is equals to floor(max / X)
+  let n = (max - 1) / divider;
+  let sum_to_n = (n * (n + 1)) / 2;
+  
+  divider * sum_to_n
+}
+
+#[test]
+fn returns_expected() {
+  assert_eq!(solution(10), 23);
+  assert_eq!(solution(11), 33);
+  assert_eq!(solution(6), 8);
+  assert_eq!(solution(std::i32::MAX/100000), 107582594);
+}
+
+fn valid_braces(s: &str) -> bool {
+    let mut stack = Vec::new();
+    for c in s.chars()
+    {
+        match c { 
+            '(' => stack.push(')'),
+            '[' => stack.push(']'),
+            '{' => stack.push('}'),
+            // works because stack.pop returns an Option
+             x => if Some(x) != stack.pop() { return false }
+        }       
+    }
+
+    stack.is_empty()
+}
+
+fn valid_braces_expect_true(s: &str)
+{
+    assert!(valid_braces(s));
+}
+fn valid_braces_expect_false(s: &str)
+{
+    assert!(!valid_braces(s));
+}
+#[test]
+fn valid_braces_tests() 
+{
+    valid_braces_expect_true("()");
+    valid_braces_expect_false("[(])");
+    valid_braces_expect_true("(){}[]");
+    valid_braces_expect_true("([{}])");
+    valid_braces_expect_false("(}");
+    valid_braces_expect_false("[(])");
+    valid_braces_expect_false("[({})](]");
+    valid_braces_expect_false("(((({{");
+}
