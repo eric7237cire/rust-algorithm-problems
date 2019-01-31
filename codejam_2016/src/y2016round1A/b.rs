e2f7f66dd2702b6a444812a088f78b8456084284
eric7237cire <eric7237cire@gmail.com>
2019-01-31 21:11:24 +0100

    small

--- codejam_2016/src/y2016round1A/b.rs
+++ codejam_2016/src/y2016round1A/b.rs
@@ -3,7 +3,7 @@ use codejam::util::codejam::run_cases;
 use bit_set::BitSet;
 use itertools::Itertools;
 use std::io::Write;
-use std::u16;
+use std::{u16,usize};
 
 /*
 
@@ -27,7 +27,7 @@ pub fn solve_all_cases()
                     .collect::<Vec<_>>();
 
                 if case_no > 3 {
-                    continue;
+                    //continue;
                 }
 
                 println!("Solving case {}", case_no);
@@ -38,16 +38,64 @@ pub fn solve_all_cases()
                     case_no,
                     solve(&papers).iter().join(" ")
                 )
-                .unwrap();
+                    .unwrap();
             }
         },
     );
 }
 
+fn backtracking( horizonal_choices: &mut Vec<usize>, all_choices: &Vec<Vec<usize>>,
+    papers: &[Vec<u16>]) -> bool 
+{
+    let N = all_choices.len();
+
+    //Reject invalid solutions
+    for column in 0..horizonal_choices.len()
+    {
+        if all_choices[column].len() == 1 {
+            continue;
+        }
+
+        let other_choice = if horizonal_choices[column] == all_choices[column][0] {
+            all_choices[column][1]
+        } else { all_choices[column][0] };
+
+        for row in 0..horizonal_choices.len()
+        {
+            if papers[horizonal_choices[row]][column] != 
+            papers[other_choice][row] {
+                return false;
+            }
+        }
+    }
+
+    if horizonal_choices.len() == all_choices.len() {
+        return true;
+    }
+
+    let current_pos = horizonal_choices.len();
+
+    for choice in all_choices[current_pos].iter()
+    {
+        horizonal_choices.push(*choice);
+        if backtracking(horizonal_choices, all_choices, papers) {
+            return true;
+        }
+        horizonal_choices.pop();
+    }
+
+    false
+}
+
+
 fn solve(papers: &[Vec<u16>]) -> Vec<u16>
 {
-    let N = papers.len();
-    let all_choices = vec![vec![-1; 2]; N];
+    let N = papers[0].len();
+    for (i, p) in papers.iter().enumerate()
+    {
+        //println!("Paper {}: {:?}", i, p);
+    }
+    let mut all_choices = vec![vec![usize::MAX; 2]; N];
 
     let mut used = BitSet::new();
     //first find the diagonal values, which must be the least value
@@ -55,25 +103,38 @@ fn solve(papers: &[Vec<u16>]) -> Vec<u16>
         let least_value = papers
             .iter()
             .enumerate()
-            .filter(|(idx, _)| !used.contains(idx))
+            .filter(|(idx, _)| !used.contains(*idx))
             .map(|(_, paper)| paper[pos])
-            .min();
+            .min().unwrap();
 
-        let choices = papers
+        //println!("Value for diag pos {} = {}", pos, least_value);
+
+        let choices: Vec<_> = papers
             .iter()
             .enumerate()
             .filter(|(idx, paper)| paper[pos] == least_value)
             .map(|(idx, _)| idx)
             .collect();
 
-        assert!(choices.len() <= 2 && choices.len() >= 1);
+        assert!(choices.len() <= 2 && !choices.is_empty());
+
+        for choice in choices.iter() {
+            used.insert(*choice);
+        }
 
         all_choices[pos] = choices;
     }
 
-    let mut ans = Vec::new();
-    ans.push(3);
-    ans.push(5);
+    //println!("All choices: {:?}", all_choices);
 
-    ans
+    let mut horizonal_choices = Vec::new();
+    backtracking(&mut horizonal_choices, &all_choices, papers);
+
+    //println!("Horizonal choices: {:?}", horizonal_choices);
+
+    //Which vertical column is missing?
+    let column_index = all_choices.iter().position( |choices| choices.len() == 1).unwrap();
+
+    horizonal_choices.iter().map( |choice| papers[*choice][column_index] ).collect()
+    
 }
