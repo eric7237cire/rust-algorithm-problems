ee8148cefb66d97ea811ba488575e728409fe826
eric7237cire <eric7237cire@gmail.com>
2019-02-03 11:50:39 +0100

    c large

--- codejam_2016/src/y2016round1B/c.rs
+++ codejam_2016/src/y2016round1B/c.rs
@@ -1,15 +1,15 @@
 use codejam::util::codejam::run_cases;
 
-use itertools::Itertools;
+//use itertools::Itertools;
 //use std::cmp::min;
 use bit_vec::BitVec;
 use std::collections::HashMap;
-use std::{i64,u16};
+use std::{usize};
 
 use std::io::Write;
-use codejam::algo::graph::flow::FlowGraph;
 /*
-
+Minimum edge cover
+Direct/Simple Bipartite implementation
 
 */
 pub fn solve_all_cases()
@@ -40,23 +40,23 @@ pub fn solve_all_cases()
     );
 }
 
-const INVALID: u16 = u16::MAX;
-const MAX_N: i16 = 1000;
+const INVALID: usize = usize::MAX;
+const MAX_N: usize = 1000;
 
 fn solve(topics: &[Vec<String>]) -> usize
 {
-    let mut first_word_ids: HashMap<&str, u16> = HashMap::new();
-    let mut second_word_ids: HashMap<&str, u16> = HashMap::new();
-    let mut first_words = vec![String::new(); MAX_N as usize];
-    let mut second_words = vec![String::new(); MAX_N as usize];
+    let mut first_word_ids: HashMap<&str, usize> = HashMap::new();
+    let mut second_word_ids: HashMap<&str, usize> = HashMap::new();
+    let mut first_words = vec![String::new(); MAX_N ];
+    let mut second_words = vec![String::new(); MAX_N];
 
 
 
-    let edges: Vec<[u16; 2]> = topics
+    let edges: Vec<[usize; 2]> = topics
         .iter()
         .map(|topic_words| {
-            let next_first_id = first_word_ids.len() as u16;
-            let next_second_id = second_word_ids.len() as u16;
+            let next_first_id = first_word_ids.len() ;
+            let next_second_id = second_word_ids.len();
 
             let first_id = *first_word_ids
                 .entry(&topic_words[0])
@@ -65,8 +65,8 @@ fn solve(topics: &[Vec<String>]) -> usize
                 .entry(&topic_words[1])
                 .or_insert(next_second_id);
 
-            first_words[ usize::from(first_id)] = topic_words[0].clone();
-            second_words[ usize::from(second_id)] = topic_words[1].clone();
+            first_words[ first_id] = topic_words[0].clone();
+            second_words[second_id] = topic_words[1].clone();
 
             /*
             println!(
@@ -88,39 +88,36 @@ fn solve(topics: &[Vec<String>]) -> usize
     let mut matchR = vec![INVALID; second_word_ids.len()];
 
     //Storing values
-    let mut queue: Vec<u16> = vec![0; first_word_ids.len()];
+    let mut queue: Vec<usize> = vec![0; first_word_ids.len()];
     let mut back = vec![0; first_word_ids.len()];
 
     let mut used_first = BitVec::from_elem(first_word_ids.len(), false);
-    let mut ans = 0;
-    let mut cur_first_word_id = 0u16;
+    
+    let mut cur_first_word_id = 0;
     while usize::from(cur_first_word_id) < first_word_ids.len() {
         let mut queueHead = 0;
         let mut queueTail = 1;
         queue[0] = cur_first_word_id;
-        used_first.set(cur_first_word_id as usize, true);
-        back[usize::from(cur_first_word_id)] = INVALID;
+        used_first.set( cur_first_word_id, true);
+        back[cur_first_word_id] = INVALID;
         //let mut found = false;
         'bfs: loop {
             assert!(queue[queueHead] != INVALID);
             let top_queue_first_word = queue[queueHead];
             queueHead += 1;
 
-            let first_to_second_edges: Vec<[u16; 2]> = edges
+            for adj_second_edge in edges
                 .iter()
-                .filter(|edge| edge[0] == top_queue_first_word)
-                .cloned()
-                .collect();
-            for (j, adj_second_edge) in first_to_second_edges.iter().enumerate() {
+                .filter(|edge| edge[0] == top_queue_first_word) {
                 assert_eq!(top_queue_first_word, adj_second_edge[0]);
 
-                let adj_second_index = adj_second_edge[1];
+                let adj_second_index = usize::from(adj_second_edge[1]);
                 //Found a non matched second index
-                if matchR[usize::from(adj_second_index)] == INVALID {
+                if matchR[adj_second_index] == INVALID {
                     let mut next_second_index = adj_second_index;
-                    let mut next_first_index = top_queue_first_word as usize;
+                    let mut next_first_index = top_queue_first_word;
 
-                    matchR[usize::from(adj_second_index)] = next_first_index as u16;
+                    matchR[adj_second_index] = next_first_index;
                     //Applying the augmenting path
                     while back[next_first_index] != INVALID {
                         assert!(back[next_first_index] != INVALID);
@@ -128,20 +125,20 @@ fn solve(topics: &[Vec<String>]) -> usize
                         let prev = back[next_first_index];
                         let pnext = matchL[next_first_index];
                         matchL[next_first_index] = next_second_index;
-                        matchR[usize::from(pnext)] = prev;
-                        next_first_index = usize::from(prev);
+                        matchR[pnext] = prev;
+                        next_first_index =prev;
                         next_second_index = pnext;
                     }
                     matchL[next_first_index] = next_second_index;
                    // found = true;
                     break 'bfs;
-                } else if !used_first[matchR[adj_second_index as usize] as usize] {
+                } else if !used_first[matchR[adj_second_index] ] {
                     //Need to find a new matching for this value, put its left index on queue
-                    used_first.set(matchR[adj_second_index as usize] as usize, true);
-                    queue[queueTail] = matchR[adj_second_index as usize];
+                    used_first.set(matchR[adj_second_index] , true);
+                    queue[queueTail] = matchR[adj_second_index];
                     queueTail += 1;
 
-                    back[matchR[adj_second_index as usize] as usize] = top_queue_first_word;
+                    back[matchR[adj_second_index] ] = top_queue_first_word;
                 }
             }
             if queueHead == queueTail {
@@ -194,7 +191,7 @@ Queue:\n{}\ntail: {}
 
         //Reset all dice values in queue
         for j in 0..queueTail {
-            used_first.set(queue[j] as usize, false);
+            used_first.set(queue[j], false);
         }
 
         cur_first_word_id += 1;
