415bd395fa618c5ac03482e866b954d4b690105d
eric7237cire <eric7237cire@gmail.com>
2019-02-03 17:32:45 +0100

    a small

--- codejam_2016/src/y2016round1C/a.rs
+++ codejam_2016/src/y2016round1C/a.rs
@@ -36,5 +36,38 @@ pub fn solve_all_cases()
 
 fn solve(P: &[u16]) -> String
 {
-    P.iter().join("")
+    let mut ans : Vec<String> = Vec::new();
+
+    let mut P: Vec< ( u16, char) > = P.iter().cloned().zip( (0..26).map(|ascii| char::from(b'A' + ascii))).collect();
+
+    let mut total: u16 = P.iter().fold(0, |acc, (count, _)| acc+count);
+
+    while total > 0 {
+
+        let upper_limit = 1 + total / 2;
+
+        println!("Count of {}.  total={} upper limit = {}",
+P.iter().map(| (ch, count)| format!("{} = {}", ch, count)).join("; "), total, upper_limit);
+
+
+        if P.iter().any( |(count, _)| *count >= upper_limit) {
+            panic!("Invalid configuration");
+        }
+
+        P.sort();
+        P.reverse();
+
+        if P[0].0 == P[1].0 && (P.len() <= 2 || P[2].0 != P[1].0) {
+            ans.push(format!("{}{}", P[0].1, P[1].1));
+            P[0].0 -= 1;
+            P[1].0 -= 1;
+        } else {
+            ans.push(P[0].1.to_string());
+            P[0].0 -= 1;
+        }
+
+        total = P.iter().fold(0, |acc, (count, _)| acc+count);
+    }
+
+    ans.iter().join(" ")
 }
