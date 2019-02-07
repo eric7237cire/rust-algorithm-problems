07bc278a51f646380d909955b7b63cfed3bda226
eric7237cire <eric7237cire@gmail.com>
2019-02-07 21:11:58 +0100

    c small

--- codejam_2016/src/y2016round2/c.rs
+++ codejam_2016/src/y2016round2/c.rs
@@ -5,7 +5,14 @@ use std::io::Write;
 //use std::mem::swap;
 
 //use codejam::util::grid::constants::*;
-use codejam::util::grid::{Grid, GridCoord, GridRowColVec, IntCoord2d};
+use codejam::util::grid::constants::EAST;
+use codejam::util::grid::constants::NORTH;
+use codejam::util::grid::constants::SOUTH;
+use codejam::util::grid::constants::WEST;
+use codejam::util::grid::{Grid, GridRowColVec, IntCoord2d};
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt;
 
 //use permutohedron::LexicalPermutation;
 
@@ -30,15 +37,15 @@ pub fn solve_all_cases()
 
                 assert_eq!(2 * (R + C), lovers.len());
 
-                if case_no > 3 {
-                    continue;
+                if case_no > 5 {
+                    //continue;
                 }
 
                 println!("Solving case {}", case_no);
 
                 writeln!(
                     buffer,
-                    "Case #{}: {}",
+                    "Case #{}:\n{}",
                     case_no,
                     solve(R, C, &lovers) //solve_brute_force(K, &prob)
                 )
@@ -46,6 +53,80 @@ pub fn solve_all_cases()
             }
         },
     );
+}
+
+fn grid_coords_to_lover(r: usize, c: usize, R: usize, C: usize) -> Option<usize>
+{
+    //top
+    if r == 0 {
+        return Some(c);
+    }
+    //right
+    if c == C + 1 {
+        return Some(r + C);
+    }
+    //bottom
+    if r == 1 + R {
+        return Some(2 * C + R - c + 1);
+    }
+    //left
+    if c == 0 {
+        return Some(2 * (R + C) - r + 1);
+    }
+
+    None
+}
+
+fn trace_path(initial_loc: IntCoord2d<usize>, grid: &Grid<String>, initial_direction: GridRowColVec) -> usize
+{
+    assert!(grid[initial_loc] == "/" || grid[initial_loc] == "\\");
+
+    //Without lover ring
+    let R = grid.R - 2;
+    let C = grid.C - 2;
+
+    let mut loc = initial_loc;
+    let mut dir = initial_direction;
+
+    loop {
+        dir = match dir {
+            NORTH => {
+                if grid[loc] == "/" {
+                    EAST
+                } else {
+                    WEST
+                }
+            }
+            SOUTH => {
+                if grid[loc] == "/" {
+                    WEST
+                } else {
+                    EAST
+                }
+            }
+            EAST => {
+                if grid[loc] == "/" {
+                    NORTH
+                } else {
+                    SOUTH
+                }
+            }
+            WEST => {
+                if grid[loc] == "/" {
+                    SOUTH
+                } else {
+                    NORTH
+                }
+            },
+            _ => panic!("odd direction")
+        };
+
+        loc += dir;
+
+        if let Some(lover) = grid_coords_to_lover(loc.0, loc.1, R, C) {
+            return lover;
+        }
+    }
 }
 
 fn solve(R: usize, C: usize, lovers: &[usize]) -> String
@@ -56,58 +137,19 @@ fn solve(R: usize, C: usize, lovers: &[usize]) -> String
     //Go through every subset
     assert!(R * C <= 16);
 
-    let mut matches = vec![ 0; 2* (R+C) + 1];
-    for matched in lovers.windows(2) {
+    let mut matches = vec![0; 2 * (R + C) + 1];
+    for matched in lovers.chunks_exact(2) {
         let L1 = matched[0];
         let L2 = matched[1];
         matches[L1] = L2;
         matches[L2] = L1;
     }
 
-    let grid_coords_to_lover = |r: usize, c: usize| {
-        //top
-        if r == 0 {
-            return Some(c);
-        }
-        //right
-        if c == C + 1 {
-            return Some(r + C);
-        }
-        //bottom
-        if r == 1 + R {
-            return Some(2 * C + R - c + 1);
-        }
-        //left
-        if c == 0 {
-            return Some(2 * (R + C) - r + 1);
-        }
-
-        None
-    };
+    debug!("Matches: {:?}", matches);
 
     for subset in 0..1 << (R * C) {
         let mut grid: Grid<String> = Grid::new(R + 2, C + 2);
-
-        //top
-        for label in 0..C {
-            grid[(0, 1 + label)] = (label + 1).to_string();
-            assert_eq!(Some(label+1), grid_coords_to_lover(0, 1+label));
-        }
-        //right
-        for label in C..C + R {
-            grid[(1 + label - C, C + 1)] = (label + 1).to_string();
-            assert_eq!(Some(label+1), grid_coords_to_lover(1 + label - C, C + 1));
-        }
-        //bottom
-        for label in C + R..2 * C + R {
-            grid[(1 + R, 2 * C + R - label)] = (label + 1).to_string();
-            assert_eq!(Some(label+1), grid_coords_to_lover(1 + R, 2 * C + R - label));
-        }
-        //left
-        for label in 2 * C + R..2 * (R + C) {
-            grid[(2 * (R + C) - label, 0)] = (label + 1).to_string();
-            assert_eq!(Some(label+1), grid_coords_to_lover(2 * (R + C) - label, 0));
-        }
+        let mut grid_garden : Grid<char> = Grid::new(R,C);
 
         for row in 0..R {
             for col in 0..C {
@@ -118,11 +160,93 @@ fn solve(R: usize, C: usize, lovers: &[usize]) -> String
                 } else {
                     "\\".to_string()
                 };
+
+                grid_garden[(row, col)] = if is_forward {
+                    '/'
+                } else {
+                    '\\'
+                };
             }
         }
 
-        debug!("Subset {:b} Grid\n{:#.4?}\n", subset, grid);
+        let mut lover_locations = Vec::new();
+
+        let mut add_lover = |r,c, label:usize, initial_dir| {
+            grid[(r, c)] = (label + 1).to_string();
+            assert_eq!(Some(label + 1), grid_coords_to_lover(r, c, R, C));
+
+            lover_locations.push(((label + 1), IntCoord2d(r, c), initial_dir));
+        };
+
+        //top
+        for label in 0..C {
+            let r = 0;
+            let c = 1 + label;
+
+            add_lover(r,c,label, SOUTH);
+        }
+        //right
+        for label in C..C + R {
+            let r = 1 + label - C;
+            let c = C + 1;
+            add_lover(r,c,label, WEST);
+        }
+        //bottom
+        for label in C + R..2 * C + R {
+            let r = 1 + R;
+            let c = 2 * C + R - label;
+            add_lover(r,c,label, NORTH);
+        }
+        //left
+        for label in 2 * C + R..2 * (R + C) {
+            let r = 2 * (R + C) - label;
+            let c = 0;
+            add_lover(r,c,label, EAST);
+        }
+
+        debug!("Subset {:b} Grid\n{:#.3?}\n", subset, grid);
+
+        let mut valid = true;
+
+        //top
+        for (lover, loc, dir) in lover_locations {
+            let other_lover = trace_path(loc + dir, &grid, dir);
+            if matches[lover] != other_lover {
+                valid = false;
+                debug!("Lover {} in location {:?} matched with {}.  Valid? {}",
+lover, loc, other_lover, valid);
+                //break;
+            }
+        }
+
+        if valid {
+            return format!("{}", g_wrapper{grid: grid_garden});
+        }
     }
 
     "IMPOSSIBLE".to_string()
+}
+
+struct g_wrapper
+{
+    grid: Grid<char>
+}
+
+
+impl Display for g_wrapper
+{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result
+    {
+        for r in 0..self.grid.R {
+            for c in 0..self.grid.C {
+                if let Err(err) = write!(f, "{}", self.grid[(r, c)]) {
+                    return Err(err);
+                }
+            }
+            if let Err(err) = writeln!(f, "") {
+                return Err(err);
+            }
+        }
+        write!(f, "")
+    }
 }
