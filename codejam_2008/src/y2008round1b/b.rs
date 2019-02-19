21576abedb136b347962fb378693a5515043d721
eric7237cire <eric7237cire@gmail.com>
2019-02-19 16:24:34 -0300

    b

--- codejam_2008/src/y2008round1b/b.rs
+++ codejam_2008/src/y2008round1b/b.rs
@@ -1,8 +1,8 @@
 use codejam::algo::graph::disjointset::DisjointSet;
-use codejam::algo::prime::sieve::SieveOfAtkin;
 use codejam::util::codejam::run_cases;
 
 use std::io::Write;
+use primal::Sieve;
 
 /*
 Disjoint set
@@ -10,17 +10,14 @@ Primes sieve
 */
 pub fn solve_all_cases()
 {
+    let sieve = Sieve::new(1_000_001);
+
     run_cases(
         &["B-small-practice", "B-large-practice"],
         "y2008round1b",
         |reader, buffer| {
             let t = reader.read_int();
 
-            let mut sieve = SieveOfAtkin::new(100_000);
-            println!("Starting sieve");
-            sieve.run();
-            let primes: Vec<u64> = sieve.get_results_vec();
-            println!("Finished sieve");
 
             for case_no in 1..=t {
                 let (a, b, p) = reader.read_tuple_3();
@@ -35,7 +32,7 @@ pub fn solve_all_cases()
                     buffer,
                     "Case #{}: {}",
                     case_no,
-                    solve(a, b, p, primes.as_slice())
+                    solve(a, b, p, &sieve)
                 )
                 .unwrap();
             }
@@ -43,7 +40,7 @@ pub fn solve_all_cases()
     );
 }
 
-fn solve(a: u64, b: u64, p_lower_bound: u64, primes: &[u64]) -> usize
+fn solve(a: u64, b: u64, p_lower_bound: u64, sieve: &Sieve) -> usize
 {
     //First step, find  P <= primes <= interval
     let lower_bound_primes = p_lower_bound;
@@ -53,7 +50,8 @@ fn solve(a: u64, b: u64, p_lower_bound: u64, primes: &[u64]) -> usize
 
     let mut ds = DisjointSet::new(interval_size);
 
-    for prime in primes.iter().filter(|p| **p >= p_lower_bound) {
+    for prime in sieve.primes_from(p_lower_bound as usize).take_while(|x| *x <= upper_bound_primes as usize) {
+        let prime = prime as u64;
         let a_mod_prime = a % prime;
         let interval_p = if a_mod_prime == 0 {
             a
@@ -65,7 +63,7 @@ fn solve(a: u64, b: u64, p_lower_bound: u64, primes: &[u64]) -> usize
 
         let set_to_merge = interval_p - a;
 
-        for i in (interval_p - a + prime..interval_size as u64).step_by(*prime as usize) {
+        for i in (interval_p - a + prime..interval_size as u64).step_by(prime as usize) {
             ds.merge_sets(i as usize, set_to_merge as usize);
         }
     }
