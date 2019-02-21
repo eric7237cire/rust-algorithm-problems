6f4b599eb230e504184f610c17be7bd321e2b525
eric7237cire <eric7237cire@gmail.com>
2019-02-21 17:30:20 -0300

    a

--- codejam_2008/src/y2008round2/a.rs
+++ codejam_2008/src/y2008round2/a.rs
@@ -1,10 +1,11 @@
 use codejam::util::codejam::run_cases;
 use std::io::Write;
 use crate::y2008round2::a::Gate::*;
+use std::cmp::min;
 
 enum Gate {
-    AND,
-    OR
+    AND = 1,
+    OR = 0
 }
 
 /*
@@ -25,7 +26,7 @@ pub fn solve_all_cases()
 
                 let interior_nodes: Vec<(Gate, bool)> = (0..(m-1) / 2).map(|_| {
                     let (g, c) : (usize,usize) = reader.read_tuple_2();
-                    (if g == 1 { AND } else { OR }, c == 1)
+                    ( g as Gate, c == 1)
                 }
                 ).collect();
 
@@ -46,7 +47,7 @@ pub fn solve_all_cases()
                     buffer,
                     "Case #{}: {}",
                     case_no,
-                    if let Some(ans) = solve(v==1,interior_nodes.as_slice(), leaf_nodes.as_slice()) {
+                    if let Some(ans) = solve(v==1,m,interior_nodes.as_slice(), leaf_nodes.as_slice()) {
                         ans.to_string()
                     } else {
                         "IMPOSSIBLE".to_string()
@@ -58,7 +59,41 @@ pub fn solve_all_cases()
     );
 }
 
-fn solve(v: bool, interior_nodes: &[(Gate, bool)], leaf_nodes: &[bool]) -> Option<usize>
+fn solve(v: bool, m: usize, interior_nodes: &[(Gate, bool)], leaf_nodes: &[bool]) -> Option<usize>
 {
+    //dp[x] = min number of changes for x to be true
+    let mut dp = vec![None; m];
+
+    for (i, ln) in leaf_nodes.iter().enumerate()
+        {
+            //if v is false we switch the input values
+            dp[i + interior_nodes.len()] = if v==ln { Some(1) } else { None} ;
+        }
+    //2n + 1; 2n + 2
+
+    for (i, int_node) in interior_nodes.iter().enumerate()
+        {
+            let gate: Gate = if v { int_node.0 } else { (1-int_node.0) as Gate };
+            let changable = int_node.1;
+            let mut min_cost = usize::MAX;
+
+            if gate == AND {
+                if let (Some(lhs), Some(rhs)) = (dp[2 * i + 1], dp[2 * i + 2]) {
+                    min_cost = lhs + rhs;
+                }
+            }
+
+            if gate == OR || changable {
+                let cost = if gate == OR {0} else {1};
+                if let Some(lhs) = dp[2*i+1] {
+                    min_cost = min(min_cost, lhs+cost);
+                }
+
+                if let Some(rhs) = dp[2*i+2] {
+                    min_cost = min(min_cost, rhs+cost);
+                }
+            }
+
+        }
     None
 }
