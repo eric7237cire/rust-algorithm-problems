8601039858a091d86020e45ab1da3228f1e199ff
eric7237cire <eric7237cire@gmail.com>
2019-02-21 22:05:39 -0300

    b

--- codejam_2008/src/y2008round2/b.rs
+++ codejam_2008/src/y2008round2/b.rs
@@ -37,34 +37,34 @@ pub fn solve_all_cases()
     );
 }
 
-fn solve(n: usize, m: usize, a: usize) -> Option<[usize;6]>
+fn solve(n: i64, m: i64, a: i64) -> Option<[i64;6]>
 {
   let narrow = min(n,m);
   let wide = max(n,m);
 
-  if (wide * narrow < A) {
+  if wide * narrow < a {
     return None;
   }
 
   let mut n1 = a / wide;
   let w2 = wide;
-  let mut w1=0;
-    let mut n2 = 0;
+  let  w1;
+    let  n2 ;
 
-  if A%wide == 0 {
+  if a%wide == 0 {
     w1 = 0;
     n2 = 0;
   } else {
     n1+=1;
     n2 = 1;
-    w1 = n1*w2 - A;
+    w1 = n1*w2 - a;
 
   }
 
-    assert_eq!(A, abs(n1 * w2 - n2 * w1) );
+    assert_eq!(a, (n1 * w2 - n2 * w1).abs() );
   //          printf("Case #%d: Xmax %d Ymax %d tar %d\n", test_case+1, N, M, A);
   //          printf("Case #%d: 0 0 %d %d %d %d\n", test_case+1, x1,y1,x2,y2);
-  if (wide == m) {
+  if wide == m {
     assert!(w1 >= 0);
     assert!(w1 <= m);
     assert!(w2 >= 0);
