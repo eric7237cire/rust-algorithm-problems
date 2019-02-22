3f6e52c645f8eb048addd5023eea7a0a0aec1347
eric7237cire <eric7237cire@gmail.com>
2019-02-22 16:37:22 -0300

    ok

--- codejam_2008/src/y2008round2/c.rs
+++ codejam_2008/src/y2008round2/c.rs
@@ -13,7 +13,7 @@ pub fn solve_all_cases()
     run_cases(
         &[
             "C-small-practice",
-            "C-large-practice"
+            //"C-large-practice"
         ],
         "y2008round2",
         |reader, buffer| {
@@ -35,8 +35,8 @@ pub fn solve_all_cases()
                     })
                     .collect();
 
-                if case_no != 9 {
-                    // continue;
+                if case_no != 1 {
+                    continue;
                 }
 
                 println!("Solving case {}", case_no);
@@ -104,12 +104,6 @@ fn solve(ships: &[Ship]) -> f64
         -x + y + z ≤ -xi + yi + zi + piY
         -x + y + z ≥ -xi + yi + zi - piY
         */
-        let mut x_interval_min = f64::MIN;
-        let mut x_interval_max = f64::MAX;
-        let mut y_interval_min = f64::MIN;
-        let mut y_interval_max = f64::MAX;
-        let mut z_interval_min = f64::MIN;
-        let mut z_interval_max = f64::MAX;
 
         for s in ships.iter() {
             let a = s.x + s.y + s.z - s.p * y;
@@ -146,56 +140,9 @@ fn solve(ships: &[Ship]) -> f64
                         debug!("H = {}", h);
             */
 
-            let x_i1 = [(a - h) as f64 / 2., (b - g) as f64 / 2.];
-            let x_i2 = [(c + e) as f64 / 2., (d + f) as f64 / 2.];
 
-            let y_i1 = [(a - f) as f64 / 2., (b - e) as f64 / 2.];
-            let y_i2 = [(c + g) as f64 / 2., (d + h) as f64 / 2.];
-
-            let z_i1 = [(a - d) as f64 / 2., (b - c) as f64 / 2.];
-            let z_i2 = [(e + g) as f64 / 2., (f + h) as f64 / 2.];
-
-            //let x = fmax(x_i1[0], x_i2[0]);
-
-            /*
-                      A - x ≤ y + z ≤ B - x
-            G + x ≤ y + z ≤ H + x
-            C - x ≤ y - z ≤ D - x
-            -F + x ≤ y - z ≤ -E + x
-            */
-            /*        debug!("A - x {} <= y+z <= B - x {}", a-x, b-x);
-            debug!("G + x {} <= y+z <= H + x {}", g+x, h+x);
-            debug!("C - x {} <= y-z <= D - x {}", c-x, d-x);
-            debug!("-F + x {} <= y-z <= -E + x {}", -f+x, -e+x);*/
-
-            x_interval_min = fmax(x_interval_min, x_i1[0]);
-            x_interval_min = fmax(x_interval_min, x_i2[0]);
-
-            x_interval_max = fmin(x_interval_max, x_i1[1]);
-            x_interval_max = fmin(x_interval_max, x_i2[1]);
-
-            y_interval_min = fmax(y_interval_min, y_i1[0]);
-            y_interval_min = fmax(y_interval_min, y_i2[0]);
-
-            y_interval_max = fmin(y_interval_max, y_i1[1]);
-            y_interval_max = fmin(y_interval_max, y_i2[1]);
-
-            z_interval_min = fmax(z_interval_min, z_i1[0]);
-            z_interval_min = fmax(z_interval_min, z_i2[0]);
-
-            z_interval_max = fmin(z_interval_max, z_i1[1]);
-            z_interval_max = fmin(z_interval_max, z_i2[1]);
-            /*
-            if !(i1[0] <= i2[1] && i2[0] <= i1[1]) {
-                debug!("Invalid!");
-                valid = false;
-                break;
-            }*/
         }
 
-        debug!("Interval X min {} max {}", x_interval_min, x_interval_max);
-        debug!("Interval Y min {} max {}", y_interval_min, y_interval_max);
-        debug!("Interval Z min {} max {}", z_interval_min, z_interval_max);
 
         let a = max_a;
         let b = min_b;
@@ -233,9 +180,12 @@ fn solve(ships: &[Ship]) -> f64
 
         valid = a <= b && c <= d && e <= f && g <= h;
 
+        /*
         valid = valid &&
             x_i1[0] <= x_i2[1] && x_i2[0] <= x_i1[1] && x_i1[0] <= x_i1[1] && x_i2[0] <= x_i2[1];
-        valid = valid
+            */
+
+/*        valid = valid
             && y_i1[0] <= y_i2[1]
             && y_i2[0] <= y_i1[1]
             && y_i1[0] <= y_i1[1]
@@ -244,7 +194,7 @@ fn solve(ships: &[Ship]) -> f64
             && z_i1[0] <= z_i2[1]
             && z_i2[0] <= z_i1[1]
             && z_i1[0] <= z_i1[1]
-            && z_i2[0] <= z_i2[1];
+            && z_i2[0] <= z_i2[1];*/
 
         //valid = x_interval_min <= x_interval_max && y_interval_min <= y_interval_max && z_interval_min <= z_interval_max;
 
