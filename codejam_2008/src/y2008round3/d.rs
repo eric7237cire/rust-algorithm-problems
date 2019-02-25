8f0276e270406a7dfdabef3d6eabe7ecd19ae54c
eric7237cire <eric7237cire@gmail.com>
2019-02-25 13:19:47 -0300

    mod inverse

--- codejam_2008/src/y2008round3/d.rs
+++ codejam_2008/src/y2008round3/d.rs
@@ -4,9 +4,9 @@ use codejam::util::vector_2d::Vector2d;
 use num_bigint::BigUint;
 use num_integer::binomial;
 use num_traits::identities::Zero;
+use std::collections::HashMap;
 use std::io::Write;
 use std::usize;
-use std::collections::HashMap;
 
 /*
 Change of base
@@ -14,10 +14,7 @@ Change of base
 pub fn solve_all_cases()
 {
     run_cases(
-        &[
-            "D-small-practice",
-                "D-large-practice"
-        ],
+        &["D-small-practice", "D-large-practice"],
         "y2008round3",
         |reader, buffer| {
             let t = reader.read_int();
@@ -64,7 +61,7 @@ fn solve(rocks_orig: &[Vector2d<isize>], n_rows: isize, n_cols: isize) -> isize
 
     let mut rocks = Vec::new();
 
-    let mut memo : HashMap<(usize,usize), usize> = HashMap::new();
+    let mut memo: HashMap<(usize, usize), usize> = HashMap::new();
 
     for r in rocks_orig.iter() {
         if let Some(rock) = change_basis(r) {
@@ -76,66 +73,72 @@ fn solve(rocks_orig: &[Vector2d<isize>], n_rows: isize, n_cols: isize) -> isize
         }
     }
 
+    rocks.sort_by(|a, b| a.c().cmp(&b.c()).then_with(|| a.r().cmp(&b.r())));
+
+    rocks.insert(0, start);
+    rocks.push(target);
+
     let mut ans = 0;
-    for subset in 0..1 << rocks.len() {
+    //always include target
+    for subset in 0..1 << (rocks.len() - 2) {
         let bs = BitVec64::with_val(subset);
         let sign = if bs.pop_count() % 2 == 0 { 1 } else { -1 };
 
-        let mut rocks_subset: Vec<Vector2d<usize>> = rocks
-            .iter()
-            .enumerate()
-            .filter(|(idx, _)| bs.get(*idx))
-            .map(|(_, rock)| *rock)
-            .collect();
+        let mut last = 0;
 
-        //rocks_subset.push(start);
-        //rocks_subset.push(target);
-
-        rocks_subset.sort_by(|a, b|
-            a.c().cmp(&b.c()).then_with(|| a.r().cmp(&b.r())));
-
-        rocks_subset.insert(0, start);
-        rocks_subset.push(target);
-
-        let ways: usize = rocks_subset
-            .windows(2)
-            .map(|win| {
-                if win[0].r() > win[1].r() {
+        let ways: usize = (1..rocks.len())
+            .filter(|&idx| idx == 0 || idx == rocks.len() - 1 || bs.get(idx - 1))
+            .map(|cur| {
+                if rocks[last].r() > rocks[cur].r() {
+                    last = cur;
                     return 0;
                 }
-                let m = win[1].r() - win[0].r();
-                let n = win[1].c() - win[0].c();
+                let m = rocks[cur].r() - rocks[last].r();
+                let n = rocks[cur].c() - rocks[last].c();
+                last = cur;
                 n_choose_k_mod(m + n, n, 10007, &mut memo)
             })
             .fold(1, |acc, w| (acc * w) % 10007);
 
-        debug!("Rocks {:?} ways {} sign {}", rocks_subset, ways, sign);
+        // debug!("Rocks {:?} ways {} sign {}", rocks_subset, ways, sign);
         ans += 10007 + sign * ways as isize;
     }
     ans % 10007
 }
 
-fn n_choose_k_mod(n: usize, k: usize, p: usize, memo: &mut HashMap<(usize,usize), usize>) -> usize
+fn n_choose_k_mod(n: usize, k: usize, p: usize, memo: &mut HashMap<(usize, usize), usize>)
+    -> usize
 {
-    let key = (n,k);
+    let key = (n, k);
     if memo.contains_key(&key) {
         return memo[&key];
     }
-    let mut n_i = BigUint::from(n);
-    let mut k_i = BigUint::from(k);
+    let mut n = n;
+    let mut k = k;
 
-    let mut product = BigUint::from(1usize);
-    let p = BigUint::from(p);
-    while k_i > BigUint::zero() {
-        product *= binomial(&n_i % &p, &k_i % &p);
-        product %= &p;
+    let mut product = 1;
+    //let p = BigUint::from(p);
+    while k > 0 {
+        let key = (n % p, k % p);
 
-        n_i /= &p;
-        k_i /= &p;
+        if memo.contains_key(&key) {
+            product *= memo[&key];
+        } else {
+            let b = biguint_to_usize(
+                &(binomial(BigUint::from(key.0), BigUint::from(key.1)) % BigUint::from(p)),
+            );
+            product *= b;
+            memo.insert(key, b);
+        }
+
+        product %= p;
+
+        n /= p;
+        k /= p;
     }
 
-    let ans = biguint_to_usize(&(product % p));
-    memo.insert(key,  ans);
+    let ans = product;
+    memo.insert(key, ans);
     ans
 }
 
@@ -177,11 +180,81 @@ fn biguint_to_usize(bu: &BigUint) -> usize
     ans
 }
 
+//https://cp-algorithms.com/algebra/module-inverse.html
+fn compute_modular_inverse(m: usize) -> Vec<usize>
+{
+    //https://cp-algorithms.com/algebra/module-inverse.html
+    let mut inv = vec![0; m + 1];
+
+    inv[1] = 1;
+    for i in 2..m {
+        inv[i] = (m - (m / i) * inv[m % i] % m) % m;
+    }
+    inv
+}
+
+//https://cp-algorithms.com/algebra/module-inverse.html
+fn compute_modular_inverse_1(a: usize, modulus: usize) -> usize
+{
+    binpow(a, modulus - 2, modulus)
+}
+
+///Binary power a^b % modulus
+/// Modulus must be prime
+fn binpow(a: usize, b: usize, modulus: usize) -> usize
+{
+    let mut a = a % modulus;
+    let mut b = b;
+    let mut res = 1;
+    while b > 0 {
+        if b & 1 > 0 {
+            res = (res * a) % modulus;
+        }
+        a = (a * a) % modulus;
+        b >>= 1;
+    }
+    return res;
+}
+
 #[cfg(test)]
 mod test_endless_knight
 {
     use super::*;
     use num_integer::binomial;
+
+    #[test]
+    fn test_modular_inverse()
+    {
+        let prime = 23;
+        let inverse = compute_modular_inverse(prime);
+
+        let numerator = [2, 2, 2, 2, 3, 3, 5, 7, 11, 13, 17, 19, 23]
+            .iter()
+            .fold(1, |acc, n| acc * *n);
+
+        for i in 2..23 {
+            //just make sure numerator is divisible by i
+            assert_eq!(0, numerator % i, "{}", i);
+            let check = (numerator / i) % prime;
+
+            //check numerator / i === inverse[i] * numerator
+            assert_eq!(check, ((numerator % prime) * inverse[i]) % prime);
+
+            assert_eq!(inverse[i], compute_modular_inverse_1(i, prime));
+        }
+    }
+
+    #[test]
+    fn test_neg_mod()
+    {
+        assert_eq!(-7 % 3, -1);
+    }
+
+    #[test]
+    fn test_binomial()
+    {
+        assert_eq!(0, binomial(2, 15));
+    }
 
     #[test]
     fn test_biguint_to_usize()
